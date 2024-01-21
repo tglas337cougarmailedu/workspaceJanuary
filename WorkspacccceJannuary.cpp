3991449e8172dc075b6c2724aa0de0a6e3122a4e
tglas337cougarmailedu <157313148+tglas337cougarmailedu@users.noreply.github.com>
2024-01-21 14:17:07 -0500

    Add files via upload

---
+++ WorkspacccceJannuary.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // Print the specified text to the console
+    std::cout << "This is my Github workspace test for January\n";
+
+    return 0; // Return 0 to indicate successful execution
+}
